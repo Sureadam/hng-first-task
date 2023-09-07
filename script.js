d04f27fa0d9694256d2e378d972aecbc9950d746
Sureadam <123544722+Sureadam@users.noreply.github.com>
2023-09-07 05:07:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Function to update the current UTC time in milliseconds
+function updateUTCTime() {
+  const utcTimeElement = document.getElementById("utcTimeMilliseconds");
+  const currentUTCTime = Date.now();
+  utcTimeElement.textContent = currentUTCTime;
+}
+
+// Function to get the current day of the week
+function getCurrentDayOfTheWeek() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDayIndex = new Date().getDay();
+  const currentDay = daysOfWeek[currentDayIndex];
+  return currentDay;
+}
+
+// Function to initialize the page
+function initializePage() {
+  updateUTCTime();
+  const currentDayElement = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+  currentDayElement.textContent = `Day of the Week: ${getCurrentDayOfTheWeek()}`;
+}
+
+// Call the initialization function when the page loads
+initializePage();
