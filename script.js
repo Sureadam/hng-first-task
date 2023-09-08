39b4debc88777277a150680c785081d8f260f547
Sureadam <123544722+Sureadam@users.noreply.github.com>
2023-09-08 01:02:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 // Function to update the current UTC time in milliseconds
 function updateUTCTime() {
   const utcTimeElement = document.getElementById("utcTimeMilliseconds");
-  const currentUTCTimeHours = new Date().getUTCHours();
-  const currentUTCTimeMins = new Date().getUTCMinutes();
-  const currentUTCTimeSecs = new Date().getUTCSeconds();
-  const currentUTCTimeMs = new Date().getUTCMilliseconds();
-  utcTimeElement.textContent = `${currentUTCTimeHours}: ${currentUTCTimeMins}: ${currentUTCTimeSecs}: ${currentUTCTimeMs}`;
+  const currentUTCTime = new Date().getTime();
+  utcTimeElement.textContent = `${currentUTCTime}`;
 }
-setInterval(updateUTCTime, 1);
+setInterval(updateUTCTime, 1000);
 // Function to get the current day of the week
 function getCurrentDayOfTheWeek() {
   const daysOfWeek = [
